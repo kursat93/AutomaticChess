fd3e38ff26629bed777449101c8d112aebe4ad28
OZLEM <ozlemkutukcu@anadolu.edu.tr>
2017-09-26 16:23:59 +0300

    ben ozlem homepages

--- app/src/main/java/com/akura/kursat/automaticchess/MainActivity.java
+++ app/src/main/java/com/akura/kursat/automaticchess/MainActivity.java
@@ -1,11 +1,17 @@
 package com.akura.kursat.automaticchess;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.akura.kursat.automaticchess.activity.HomePage;
 import com.akura.kursat.automaticchess.model.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button homepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         User user =new User();
         user.getUser();
+
+
+        homepage=(Button)findViewById(R.id.btnHomePage);
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getBaseContext(),HomePage.class);
+                startActivity(i);
+
+            }
+        });
+
 
 
 
