850cba505d9ac6047dc4ace5a4a1a4e42a333e1e
Kürşat <kursat93@users.noreply.github.com>
2017-10-16 18:24:31 +0300

    oyun eklendi firebase güncelliyor.  X  player turn düzenlenecek ///oldu sanırım  -oyun girişi tamamen düzenlecenek  -X? oyun bittiğinde oda silinecek  -karşı tarafa check mate gibi bildiriler gönderilecek  -promote yapılacak  -kazanılan ve oynanan oyunların skoru tutulacak
     new FirebaSE

--- app/src/main/java/com/akura/kursat/automaticchess/activity/CreateGameActivity.java
+++ app/src/main/java/com/akura/kursat/automaticchess/activity/CreateGameActivity.java
@@ -94,7 +94,6 @@ public class CreateGameActivity extends AppCompatActivity {
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 createRoom();
 
 
